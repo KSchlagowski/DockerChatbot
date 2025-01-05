5f5280a6066df99db9f6395ce8decd7cb397ba7c
KSchlagowski <kamilschlagowski@gmail.com>
2025-01-05 12:28:29 +0100

    Refactor code and make AI find by itself correct doc article to explain to user.

--- main.py
+++ main.py
@@ -1,78 +1,20 @@
-import logging
-
 from dotenv import load_dotenv
 from openai import OpenAI
 import requests
 from bs4 import BeautifulSoup
-import json
+import time
 
 load_dotenv()
 client = OpenAI()
-
-tools = [
-  {
-      "type": "function",
-      "function": {
-          "name": "docker_installation",
-          "description": "Call this function whenever user asks you about Docker installation. This tool will give you necessary information about this case.",
-          "parameters": {
-              "type": "object",
-              "properties": {
-                  "operation_system": {"type": "string"}
-              },
-              "required": ["operation_system"]
-          },
-      },
-  }
-]
 
 
 def scrape_website(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     s = soup.find('article', class_='prose min-w-0 flex-[2_2_0%] max-w-4xl dark:prose-invert')
-    site_content = soup.find_all('p')
+    site_content = s.find_all('p')
     return site_content
 
-
-def get_website_content_based_on_tool_call(tool_call):
-    content = "Error - the website wasn't found or the function wasn't properly called."
-    if tool_call.function.name == "docker_installation":
-        os = json.loads(tool_call.function.arguments)['operation_system']
-        os = str(os).lower()
-        if os == "mac":
-            content = scrape_website('https://docs.docker.com/desktop/setup/install/mac-install/')
-        elif os == "windows":
-            content = scrape_website('https://docs.docker.com/desktop/setup/install/windows-install/')
-        elif os == "linux":
-            content = scrape_website('https://docs.docker.com/desktop/setup/install/linux/')
-    return content
-
-
-def process_query(message):
-    completion = client.chat.completions.create(
-      model="gpt-4o-mini",
-      messages=[
-          {
-              "role": "system",
-              "content": "You are a helpful Docker customer support assistant. Use the supplied tools to assist the user on issues related to Docker application."
-          },
-          {
-              "role": "system",
-              "content": "Use the 'docker_installation' function when the user asks about installation of Docker."
-          },
-          {"role": "user", "content": f"{message}"}],
-      tools=tools,
-    )
-
-    website_content = "Error - no website content"
-    try:
-        for tool_call in completion.choices[0].message.tool_calls:
-            website_content = get_website_content_based_on_tool_call(tool_call)
-    except Exception as e:
-        logging.error(f"Error occurred while fetching function call: {str(e)}")
-
-    return website_content
 
 def print_model_response(message, response):
     stream = client.chat.completions.create(
@@ -81,9 +23,16 @@ def print_model_response(message, response):
             {"role": "user", "content": f"{message}"},
             {
                 "role": "system",
-                "content": f"You are a helpful Docker customer support assistant. Answer only to questions related to Docker. When the question is not about Docker kindly inform user that you can't answer it. Base your knowledge only on provided tools. Your tools gave you this docs website content to help you answer user's question: {response}. If previous response was an error, kindly inform user that he should try again later."
+                "content": f"You are a helpful Docker customer support assistant. Answer only to questions related to "
+                           f"Docker. When the question is not about Docker kindly inform user that you can't answer "
+                           f"it. Base your knowledge only on provided tools. Your tools gave you this docs website "
+                           f"content to help you answer user's question: {response}. If previous response was an "
+                           f"error, kindly inform user that he should try again later. "
+                           f"Focus on giving the user information from the documentation (your tools rely on it). "
+                           f"If information from the documentation requires additional explanation, make it clear to "
+                           f"the user that this is your opinion and not a description of the documentation."
             }
-            ],
+        ],
         stream=True,
     )
 
@@ -92,11 +41,58 @@ def print_model_response(message, response):
             print(chunk.choices[0].delta.content, end="")
 
 
+def look_for_proper_doc_article(user_prompt):
+    r = requests.get('https://docs.docker.com/desktop/setup/install/linux/')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    s = soup.find('nav', class_='md:text-sm flex flex-col')
+    site_content = s.find_all('a')
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user",
+             "content": f"What is the link for an article that can help user in his Docker problem? "
+                        f"User's problem: {user_prompt}. Nav content: {site_content}. "
+                        f"Give me only the URL link, no other information. "
+                        f"Don't hallucinate, give me exact link that is provided in nav content. "},
+        ]
+    )
+    doc_article_url = completion.choices[0].message.content
+    print("Found url: ", doc_article_url)
+    return doc_article_url
+
+
+def process_query(message):
+    start = time.time()
+
+    doc_url = look_for_proper_doc_article(message)
+    article = scrape_website(doc_url)
+    print_model_response(message, article)
+
+    end = time.time()
+    exec_time = end - start
+    return exec_time
+
+
+# test
+sample_prompts = ["How to install Docker on Mac?", "How to use docker-compose?",
+                  "What is a container? How to run one?",
+                  "How to setup Docker for cloud?", "Is Docker free?"
+]
+
+for prompt in sample_prompts:
+    print()
+    print("Prompt = ", prompt)
+    print()
+    process_time = process_query(prompt)
+    print()
+    print("Time needed to process this query is ", process_time, " seconds.")
+    print("---------------------------------")
+
 if __name__ == "__main__":
     while True:
-        message = input(">>> ")
-        if message == "/exit":
+        query = input(">>> ")
+        if query == "/exit":
             break
-        response = process_query(message)
-        print_model_response(message, response)
+        process_query(query)
         print()
